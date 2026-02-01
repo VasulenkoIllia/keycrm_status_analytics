2f9ba7256e9a0c03e37d9190df7c48af4db083a7
Illia Vasulenko <vasulenko.illia@gmail.com>
2026-02-02 01:47:53 +0200

    fix redis

--- server/src/db/redis.js
+++ server/src/db/redis.js
@@ -4,7 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 export function createRedisClients() {
-  const url = `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`;
+  const url =
+    process.env.REDIS_URL ||
+    `redis://${process.env.REDIS_HOST || 'localhost'}:${process.env.REDIS_PORT || 6379}`;
   const pub = createClient({ url });
   const sub = createClient({ url });
   return { pub, sub };
